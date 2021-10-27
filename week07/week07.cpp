a73bf84d252c6910b45bac5d229077febb844aa4
Adrian Whetten <awhett@byui.edu>
2021-10-27 15:50:54 -0600

    Setup

--- week07/week07.cpp
+++ week07/week07.cpp
@@ -13,181 +13,114 @@
 *********************************************************************/
 
 #include <iostream>
+#include <iomanip>
 #include <string>
+using namespace std;
 
-/*********************************************************************
-*  displayMenu()
-*  Called by interact()
-*  Displays menu options.
-*********************************************************************/
-void displayMenu()
-{
-    std::cout << "OPTIONS:\n"
-        << "   A  Option 1\n"
-        << "   B  Option 2\n"
-        << "   Q  Quit\n";
 
-    return;
-}
-
-/*********************************************************************
-*  displayPrompt()
-*  Displays passed message.
-*********************************************************************/
-void displayPrompt(std::string message)
-{
-    std::cout << message << "\n";
-    return;
-}
-
-/*********************************************************************
-*  displayResult()
-*  Called by interact()
-*  Displays results.
-*********************************************************************/
-void displayResult(std::string message)
-{
-    std::cout << "TEST RESULT: " << message << "\n\n";
-    return;
-}
-
-/*********************************************************************
-*  displayError()
-*  Called by interact()
-*  Displays error message.
-*********************************************************************/
-void displayError(std::string message)
-{
-    std::cout << message << "\n\n";
-    return;
-}
-
-/*********************************************************************
-*  displayHeader()
-*  Called by interact()
-*  Displays app identifier.
-*********************************************************************/
-void displayHeader()
-{
-    std::cout << "\n************ CSE 453 ************\n\n";
-    return;
-}
-
-/*********************************************************************
-*  clearScreen()
-*  Called by interact()
-*  Clears the screen.
-*********************************************************************/
-void clearScreen()
-{
-    if (system("CLS")) { system("clear"); }
-    return;
-}
-
-/*********************************************************************
-*   interact(), until user types "Q".
-*   Called by main.
-*********************************************************************/
-void interact()
-{
-    clearScreen();
-    displayHeader();
-    displayMenu();
-
-    char answer[2] = "";
-    std::string returnString;
-    do
-    {
-        if (std::cin.fail()) // bad input
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-
-        std::cin.getline(answer, 2);
-        if (islower(answer[0])) { answer[0] = toupper(answer[0]); }
-        returnString.clear();
-        switch (answer[0])
-        {
-        case '\0': // no input
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Invalid command");
-            displayMenu();
-            break;
-
-        case 'A':
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Not implemented");
-            displayMenu();
-            break;
-
-        case 'B':
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Not implemented");
-            displayMenu();
-            break;
-
-        case 'Q': // Quit
-            clearScreen();
-            break;
-
-        default:
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Invalid command");
-            displayMenu();
-            break;
-        }
-    } while (answer[0] != 'Q' && answer[0] != 'q');
-
-    return;
-}
-
-/*********************************************************************
-*   Main driver of the program
-*********************************************************************/
+/**********************************************
+ * MAIN : The top of the callstack.
+ **********************************************/
 int main()
 {
-    //Russells_commit
-    //Calebs_commit
-    interact();
+    char text[8] = "*MAIN**";
+    long number = 123456;
+    void (*pointerFunction)() = fail;
+    const char* message = failMessage;
+
+    // display the initial values of the local variables
+    cout << "main() : " << (void*)main << endl;
+    cout << "\ttext:             " << text << endl;
+    cout << "\tnumber:           " << number << endl;
+    cout << "\tmessage:          " << message << endl;
+    cout << "\tfunction pointer: ";
+    pointerFunction();
+
+    // call the other functions
+    one(number + 111111);     // 234567
+
+    // display the new values of the local variables
+    cout << "main() - after\n";
+    cout << "\ttext:             " << text << endl;
+    cout << "\tnumber:           " << number << endl;
+    cout << "\tmessage:          " << message << endl;
+    cout << "\tfunction pointer: ";
+    pointerFunction();
 
     return 0;
 }
 
-/*************************************
- * ARRAY VULNERABILTY
- * 1. There must be an array and an array index variable
- * 2. The array index variable must be reachable through external input.
- * 3. There must not be bounds checking on the array index variable.
- ****************************************/
-void arrayVulnerability(/* feel free to add parameters */)
+/************************************************
+ * CONVERT TO STRING
+ * Convert the data from p into a human-readable string
+ * by removing all the unprintable characters and replacing
+ * them with a dot
+ ***********************************************/
+string displayCharArray(const char* p)
 {
+    string output;
+    for (int i = 0; i < 8; i++)
+        output += string(" ") + (p[i] >= ' ' && p[i] <= 'z' ? p[i] : '.');
+    return output;
 }
 
-/**************************************
- * ARRAY WORKING
- * Call arrayVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void arrayWorking()
+/**********************************************
+ * ONE : The next item on the call stack
+ **********************************************/
+void one(long number)               // 234567
 {
-    arrayVulnerability(/* parameters with non-malicious data */);
+    char text[8] = "**ONE**";
+
+    cout << "one() : " << (void*)one << endl;
+    cout << "\tmessage: " << (void*)failMessage << endl;
+    cout << "\tfail():  " << (void*)fail << endl;
+
+    two(number + 111111);    // 345678
 }
 
-/**************************************
- * ARRAY EXPLOIT
- * 1. The attacker provides an array index value outside the expected range
- * 2. The attacker must be able to provide input or redirect
- *    existing input into the array at the index he provided
- * 3. The injected value must alter program state in a way
- *    that is desirable to the attacker
- *************************************/
-void arrayExploit()
+/**********************************************
+ * TWO : The bottom of the call stack
+ **********************************************/
+void two(long number)              // 345678
 {
-    arrayVulnerability(/* parameters with malicious data */);
+    // start your display of the stack from this point
+    long bow = number + 111111;     // 456789
+    char text[8] = "**TWO**";
+    long* pLong = NULL;
+    char* pChar = NULL;
+
+    // header for our table. Use these setw() offsets in your table
+    cout << '[' << setw(2) << 'i' << ']'
+        << setw(15) << "address"
+        << setw(20) << "hexadecimal"
+        << setw(20) << "decimal"
+        << setw(18) << "characters"
+        << endl;
+    cout << "----+"
+        << "---------------+"
+        << "-------------------+"
+        << "-------------------+"
+        << "-----------------+\n";
+    for (long i = 24; i >= -4; i--)   // You may need to change 24 to another number
+    {
+        ////////////////////////////////////////////////
+        // Insert code here to display the callstack
+
+        //
+        ////////////////////////////////////////////////
+    }
+
+    ////////////////////////////////////////////////
+    // Insert code here to change the variables in main()
+
+    // change text in main() to "*main**"
+
+    // change number in main() to 654321
+
+    // change pointerFunction in main() to point to pass
+
+    // change message in main() to point to passMessage
+
+    //
+    ////////////////////////////////////////////////
 }

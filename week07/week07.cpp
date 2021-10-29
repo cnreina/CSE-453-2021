a0c1c089c059356f61f86ff341ad705d9efc69da
Caleb Georgeson <caleb@home>
2021-10-28 18:23:14 -0600

    Finished week07 lab

--- week07/week07.cpp
+++ week07/week07.cpp
@@ -1,160 +1,147 @@
-
-/*********************************************************************
-*   CSE 453
-* 
-*   Carlos N Reina
-*   Adrian Whetten
-*   Jordan Burdett 
-*   Caleb Georgeson 
-*   Russell Roberts 
-*   Stephen Ellis 
-*   Orion Christensen    
-*
-*********************************************************************/
-
 #include <iostream>
+#include <iomanip>
 #include <string>
+using namespace std;
 
-/*********************************************************************
-*  displayMenu()
-*  Called by interact()
-*  Displays menu options.
-*********************************************************************/
-void displayMenu()
-{
-    std::cout << "OPTIONS:\n"
-        << "   A  Option 1\n"
-        << "   B  Option 2\n"
-        << "   Q  Quit\n";
+void one(long number);
+void two(long number);
+void pass() { cout << "You pass :)\n"; }
+void fail() { cout << "You've failed :(\n"; }
+const char * passMessage = ":)";
+const char * failMessage = ":(";
 
-    return;
-}
-
-/*********************************************************************
-*  displayPrompt()
-*  Displays passed message.
-*********************************************************************/
-void displayPrompt(std::string message)
-{
-    std::cout << message << "\n";
-    return;
-}
-
-/*********************************************************************
-*  displayResult()
-*  Called by interact()
-*  Displays results.
-*********************************************************************/
-void displayResult(std::string message)
-{
-    std::cout << "TEST RESULT: " << message << "\n\n";
-    return;
-}
-
-/*********************************************************************
-*  displayError()
-*  Called by interact()
-*  Displays error message.
-*********************************************************************/
-void displayError(std::string message)
-{
-    std::cout << message << "\n\n";
-    return;
-}
-
-/*********************************************************************
-*  displayHeader()
-*  Called by interact()
-*  Displays app identifier.
-*********************************************************************/
-void displayHeader()
-{
-    std::cout << "\n************ CSE 453 ************\n\n";
-    return;
-}
-
-/*********************************************************************
-*  clearScreen()
-*  Called by interact()
-*  Clears the screen.
-*********************************************************************/
-void clearScreen()
-{
-    if (system("CLS")) { system("clear"); }
-    return;
-}
-
-/*********************************************************************
-*   interact(), until user types "Q".
-*   Called by main.
-*********************************************************************/
-void interact()
-{
-    clearScreen();
-    displayHeader();
-    displayMenu();
-
-    char answer[2] = "";
-    std::string returnString;
-    do
-    {
-        if (std::cin.fail()) // bad input
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-
-        std::cin.getline(answer, 2);
-        if (islower(answer[0])) { answer[0] = toupper(answer[0]); }
-        returnString.clear();
-        switch (answer[0])
-        {
-        case '\0': // no input
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Invalid command");
-            displayMenu();
-            break;
-
-        case 'A':
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Not implemented");
-            displayMenu();
-            break;
-
-        case 'B':
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Not implemented");
-            displayMenu();
-            break;
-
-        case 'Q': // Quit
-            clearScreen();
-            break;
-
-        default:
-            clearScreen();
-            displayHeader();
-            displayError("ERROR: Invalid command");
-            displayMenu();
-            break;
-        }
-    } while (answer[0] != 'Q' && answer[0] != 'q');
-
-    return;
-}
-
-/*********************************************************************
-*   Main driver of the program
-*********************************************************************/
+/**********************************************
+ * MAIN : The top of the callstack.
+ **********************************************/
 int main()
 {
-    //Russells_commit
-    //Calebs_commit
-    interact();
+   char text[8] = "*MAIN**";
+   long number = 123456;
+   void (*pointerFunction)() = fail;
+   const char * message = failMessage;
 
-    return 0;
+   // display the initial values of the local variables
+   cout << "main() : " << (void *)main << endl;
+   cout << "\ttext:             " << text              << endl;
+   cout << "\tnumber:           " << number            << endl;
+   cout << "\tmessage:          " << message           << endl;
+   cout << "\tfunction pointer: ";
+   pointerFunction();
+
+   // call the other functions
+   one(number + 111111);     // 234567
+
+   // display the new values of the local variables
+   cout << "main() - after\n";
+   cout << "\ttext:             " << text              << endl;
+   cout << "\tnumber:           " << number            << endl;
+   cout << "\tmessage:          " << message           << endl;
+   cout << "\tfunction pointer: ";
+   pointerFunction();
+
+   return 0;
+}
+
+/************************************************
+ * CONVERT TO STRING
+ * Convert the data from p into a human-readable string
+ * by removing all the unprintable characters and replacing
+ * them with a dot
+ ***********************************************/
+string displayCharArray(const char * p)
+{
+   string output;
+   for (int i = 0; i < 8; i++)
+       output += string(" ") + (p[i] >= ' ' && p[i] <= 'z' ? p[i] : '.');
+   return output;
+}
+
+string longToHexString(long number)
+{
+    stringstream stream;
+    stream << hex << number;
+
+    return "0x" + stream.str();
+}
+
+/**********************************************
+ * ONE : The next item on the call stack
+ **********************************************/
+void one(long number)               // 234567
+{
+   char text[8] = "**ONE**";
+
+   cout << "one() : " << (void *)one << endl;
+   cout << "\tmessage: " << (void *)failMessage << endl;
+   cout << "\tfail():  " << (void *)fail        << endl;
+
+   two(number + 111111);    // 345678
+}
+
+/**********************************************
+ * TWO : The bottom of the call stack
+ **********************************************/
+void two(long number)              // 345678
+{
+   // start your display of the stack from this point
+   long bow = number + 111111;     // 456789
+   char text[8] = "**TWO**";
+   long * pLong = NULL;
+   char * pChar = NULL;
+
+   // header for our table. Use these setw() offsets in your table
+   cout << '[' << setw(2) << 'i' << ']'
+        << setw(15) << "address"
+        << setw(20) << "hexadecimal"
+        << setw(20) << "decimal"
+        << setw(18) << "characters"
+        << endl;
+   cout << "----+"
+        << "---------------+"
+        << "-------------------+"
+        << "-------------------+"
+        << "-----------------+\n";
+   for (long i = 30; i >= -4; i--)   // You may need to change 24 to another number
+   {
+      ////////////////////////////////////////////////
+      // Insert code here to display the callstack
+        pLong = &bow + i;
+        pChar = (char *) pLong;
+
+        cout << '[' << setw(2) << i << ']'
+             << setw(15) << pLong
+             << setw(20) << longToHexString(*pLong)
+             << setw(20) << *pLong
+             << setw(18) << displayCharArray(pChar)
+             << endl;
+      //
+      ////////////////////////////////////////////////
+   }
+
+   ////////////////////////////////////////////////
+   // Insert code here to change the variables in main()
+                                                                                
+   // change text in main() to "*main**"
+    pLong = &bow + 28;
+    pChar = (char *) pLong;
+    pChar[1] = 'm';
+    pChar[2] = 'a';
+    pChar[3] = 'i';
+    pChar[4] = 'n';
+    
+   // change number in main() to 654321
+   pLong = &bow + 25;
+   *pLong = 654321;
+
+   // change pointerFunction in main() to point to pass
+   pLong = &bow + 26;
+   *pLong = (long) &pass;
+
+   // change message in main() to point to passMessage
+   pLong = &bow + 27;
+   *pLong = (long) passMessage;
+
+   //cout << pPointAtMessage << endl;
+   //
+   ////////////////////////////////////////////////
 }

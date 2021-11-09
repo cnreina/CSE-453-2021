fd85f16d9484702d1ef137c4fefa894017666674
Carlos N Reina <cnreina@gmail.com>
2021-11-09 15:17:56 -0700

    added tests folder

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,3 @@
-
 /*********************************************************************
 *   CSE 453
 *
@@ -22,5 +21,23 @@ using namespace std;
  ********************************************************************/
 int main()
 {
-    return 0;
+    // get the secret
+   string secret;
+   cout << "What is the secret?   ";
+   cin  >> secret;
+   // get the password
+   string password;
+   cout << "What is the password? ";
+   cin  >> password;
+   // now go in a holding pattern until the user provides the correct password
+   string prompt;
+   do
+   {
+      cout << "You can get the secret only with the password: ";
+      cin  >> prompt;
+   }
+   while (prompt != password);
+   // reveal the secret
+   cout << "The secret is " << secret << endl;      
+   return 0;
 }
